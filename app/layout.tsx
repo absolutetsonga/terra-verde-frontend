6849355fcea09528981521892c51655b83a34fc1
absolutetsonga <abstsonga@gmail.com>
2024-02-03 19:00:58 +0600

    modified: initialized redux toolkit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,8 @@
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { StoreProvider } from "@/src/01_app/providers/StoreProvider";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Terra Verde",
-  description: "Мы - улучшаем экосистему нашей страны!",
-};
 
 export default function RootLayout({
   children,
@@ -15,8 +10,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <StoreProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </StoreProvider>
   );
 }
