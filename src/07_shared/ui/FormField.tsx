e6bfc6c824dcc8ce410b88523a7a600a08774f4e
absolutetsonga <abstsonga@gmail.com>
2024-02-04 15:52:54 +0600

    form layout changed

--- src/07_shared/ui/FormField.tsx
+++ src/07_shared/ui/FormField.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 
 import { UseFormRegister } from "react-hook-form";
 import { ITreePoint } from "../lib/models";
@@ -29,71 +30,74 @@ const FormField = ({
   const [imagePreview, setImagePreview] = useState("");
 
   return (
-    <div>
-      <div className="flex flex-col gap-2">
-        <div className="flex justify-between">
-          <label htmlFor={name} className="form-label">
-            {text}
-          </label>
-          {link && (
-            <Link href={link.linkUrl}>
-              <a className="form-link">{link.linkText}</a>
-            </Link>
-          )}
-        </div>
+    <div className="flex flex-col gap-2">
+      <div className="w-full">
+        <label htmlFor={name} className="form-label">
+          {text}
+        </label>
+        {link && (
+          <Link href={link.linkUrl}>
+            <a className="form-link">{link.linkText}</a>
+          </Link>
+        )}
+      </div>
 
-        <div>
-          {type === "message" && (
-            <textarea
-              id={name}
-              {...register(name)}
-              placeholder={placeholder}
-              className="form-textarea"
-            />
-          )}
+      <div className="flex flex-col gap-2 info-container">
+        {type === "message" && (
+          <textarea
+            id={name}
+            {...register(name)}
+            placeholder={placeholder}
+            className="form-textarea"
+          />
+        )}
 
-          {type === "file" && (
-            <input
-              id={name}
-              type={type}
-              {...register(name, {
-                onChange: (event) => {
-                  const file = event.target.files[0];
-                  if (file) {
-                    const reader = new FileReader();
-                    reader.onload = (e) => {
-                      if (typeof e.target?.result === "string") {
-                        setImagePreview(e.target.result);
-                      }
-                    };
-                    reader.readAsDataURL(file);
-                  }
-                },
-              })}
-              placeholder={placeholder}
-              className="form-input"
-            />
-          )}
+        {type === "file" && (
+          <input
+            id={name}
+            type={type}
+            {...register(name, {
+              onChange: (event) => {
+                const file = event.target.files[0];
+                if (file) {
+                  const reader = new FileReader();
+                  reader.onload = (e) => {
+                    if (typeof e.target?.result === "string") {
+                      setImagePreview(e.target.result);
+                    }
+                  };
+                  reader.readAsDataURL(file);
+                }
+              },
+            })}
+            placeholder={placeholder}
+            className="form-input"
+          />
+        )}
 
-          {type === "input" && (
-            <input
-              id={name}
-              type={type}
-              {...register(name)}
-              placeholder={placeholder}
-              className="form-input"
-              step="any"
-            />
-          )}
+        {type === "input" && (
+          <input
+            id={name}
+            type={type}
+            {...register(name)}
+            placeholder={placeholder}
+            className="form-input"
+            step="any"
+          />
+        )}
 
+        {errorMessage && <p className="error-message">{errorMessage}</p>}
+
+        <div className="flex items-center justify-center">
           {imagePreview && (
-            <img
+            <Image
               src={imagePreview}
               alt="Preview"
+              width={400}
+              height={400}
               className="form-image-preview"
             />
           )}
-          {errorMessage && <p className="error-message">{errorMessage}</p>}
         </div>
       </div>
     </div>
